793da7ba904fa22b2b866b9e418220f572e6e162
LokeshN1 <lokeshnegi399@gmail.com>
2024-08-25 12:37:56 +0530

    updating cookie

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(session({
     secret: 'your-secret-key',  // Replace with a strong secret key
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }  // Set to true if using HTTPS
+    cookie: { secure: true }  // Set to true if using HTTPS
 }));
 
 app.use(express.json());
