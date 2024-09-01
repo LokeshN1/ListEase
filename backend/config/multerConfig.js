85090d799686355e359e9fd94212a5036601b859
LokeshN1 <lokeshnegi399@gmail.com>
2024-09-01 11:41:53 +0530

    defining path

--- backend/config/multerConfig.js
+++ backend/config/multerConfig.js
@@ -1,12 +1,13 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
+const path = require('path'); // Import the path module to use path.extname
 
-// Configure Cloudinary -> for uploading photos and files in cloud
+// Configure Cloudinary
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET // Replace with your API secret
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 // Configure Multer to use Cloudinary Storage
@@ -19,23 +20,24 @@ const storage = new CloudinaryStorage({
   },
 });
 
+// Multer middleware for uploading profile pictures
 const uploadProfilePicture = multer({
-
   storage: storage,
   limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
   fileFilter: (req, file, cb) => {
-    console.log("User profile picture uploading!")
+    console.log("User profile picture uploading!");
     const filetypes = /jpeg|jpg|png/;
     const mimetype = filetypes.test(file.mimetype);
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
     if (mimetype && extname) {
       return cb(null, true);
     } else {
-      cb(new Error('Only images are allowed'));
+      cb(new Error('Only images are allowed (jpeg, jpg, png)'));
     }
   },
 });
 
+// Multer middleware for uploading Excel files
 const uploadExcel = multer({
   storage: multer.memoryStorage(), // Store Excel files in memory
   limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 10MB
