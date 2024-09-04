439c26c906c406a1fa1655748d57cd6fca004a58
LokeshN1 <lokeshnegi399@gmail.com>
2024-09-04 21:44:30 +0530

    logging datatype of profile picture 2

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -207,7 +207,7 @@ const updateUserProfilePicture = async (req, res) => {
       const previousImgUrl = req.user.profilePicture;
 
     console.log("Info about profile picture also its datatype"+ previousImgUrl+"\t"+typeof(previousImgUrl));
-    if(previousImgUrl){ // if their exist a profile picture 
+    if(previousImgUrl != undefined){ // if their exist a profile picture 
   // first delete the previous profile picture from cloudnary
       const publicId = extractPublicId(previousImgUrl);
       await deleteImageFromCloudinary(publicId);
